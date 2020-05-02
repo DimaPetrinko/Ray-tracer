fd14204318b7f34aad9f17d5f48c4c50c2f8b5d3
Dmytrii Petrinko <petrinko6@gmail.com>
2020-05-02 23:27:04 +0300

    Added writing data into bmp image

--- Ray-tracer/src/main.cpp
+++ Ray-tracer/src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "bmp.hpp"
 
-struct Pixel
+struct Color
 {
-    float r, g, b;
+    uint32_t c;
+
+    Color(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 0xff)
+    {
+        c = a | b << 8 | g << 16 | r << 24;
+    }
+
+    uint8_t r() { return c >> 24; }
+    uint8_t g() { return c >> 16; }
+    uint8_t b() { return c >> 8; }
+    uint8_t a() { return c; }
 };
 
 struct Vec3
@@ -63,17 +74,20 @@ const int screenWidth = 800;
 const int screenHeight = 600;
 const Vec2 clipPlanes = {1, 1000};
 
-Pixel image[screenWidth * screenHeight] = {};
-
 int main()
 {
     Camera camera{position : {0, 0, 0}, direction : {0, 0, -1}};
 
+    BMP bmp2(800, 600);
     for (int x = 0; x < screenWidth; x++)
     {
         for (int y = 0; y < screenHeight; y++)
         {
-            image[FlattenIndex(x, y, screenWidth)] = {1, 1, 1};
+            Color c{0xff, 0xa0, 0x00};
+
+            auto r = c.r();
+            bmp2.set_pixel(x, y, c.b(), c.g(), c.r(), c.a());
         }
     }
+    bmp2.write("Ray-tracer/res/img_test.bmp");
 }
